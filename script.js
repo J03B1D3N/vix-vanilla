4a7bf4db8ab70e7f8c39e91c1cb02e39d90763fd
Justas Lapiene <Justas63@gmail.com>
2023-04-07 18:33:54 +0200

    dynamic variables now create correctly

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ async function ApiCall() {
 
     const apiCall = await fetch('https://www.wix.com/_serverless/hiring-task-spreadsheet-evaluator/sheets')
 
-    const data = await apiCall.json()
+    const processedData = await apiCall.json()
 
     // console.log(data)
 
@@ -14,17 +14,63 @@ async function ApiCall() {
         )
     }
 
-    const data2 = data.sheets[1].data[0]
+    const information = processedData.sheets
      
-    console.log(data2)
+    // console.log(information)
     // console.log(StringifyObject())
 
-    for(i = 0; i < data2.length; i++) {
-        eval('var ' + `A${i + 1}` + '= ' + data2[i] + ";")
-        console.log('done', i +1)
-    }
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+
+    const dummy = information[7]
+    console.log(dummy)
+
+    const example = eval("=52")
+    console.log(example)
     
-    console.log(A1)
+        //     for(let data = 0; data < dummy.data.length; data++){
+        //         console.log('ARRAY ' + data)
+    
+        //         for(let element = 0; element < dummy.data[data].length; element++) {
+    
+        //             console.log('element ' + element)
+
+        //             eval('var ' + alphabet[element] + (data + 1) + '= ' + 'dummy.data[data][element]' + ";")
+        //         }
+        //     }
+
+
+        // console.log(A1, B1, C1, D1)
+        // console.log(A2, B2, C2, D2)
+    
+    
+    
+    
+    
+    
+    // for(sheet = 0;sheet < information.length; sheet++) {
+    //     console.log('SHEET ' + sheet)
+
+    //     for(let data = 0; data < information[sheet].data.length; data++){
+    //         console.log('ARRAY ' + data)
+
+    //         for(let element = 0; element < information[sheet].data[data].length; element++) {
+
+    //             console.log('element ' + element)
+    //             // eval('var ' + alphabet[i] + (c + 1) + '= ' + data2[c][i] + ";")
+    
+    //         }
+
+    //     }
+    // }
+  
+    
+
+       
+    // }
+
+    
+    
+    // console.log(A1, B1, C1, D1)
 
     
 }
